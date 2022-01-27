cf88f20b24ce3985ae0baf3647339c3c0ceff9ab
Muluneh Derese <muluneh.derese@protonmail.com>
2022-01-27 22:13:05 +0300

    folder cleanup: namespaces

---
+++ src/drag-drop-interfaces.ts
@@ -0,0 +1,13 @@
+// Drag & drop interfaces
+namespace App {
+  export interface Draggable {
+    dragStartHandler(event: DragEvent): void;
+    dragEndHandler(event: DragEvent): void;
+  }
+
+  export interface DragTarget {
+    dragOverHandler(evet: DragEvent): void;
+    dropHandler(event: DragEvent): void;
+    dragLeaveHandler(event: DragEvent): void;
+  }
+}
---
+++ src/project-model.ts
@@ -0,0 +1,15 @@
+// Project Type {
+enum ProjectStatus {
+  Active,
+  Finished,
+}
+
+class Project {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public people: number,
+    public status: ProjectStatus
+  ) {}
+}
