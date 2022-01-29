34f049478172e195d4d786b94310956b471a9a41
Muluneh Derese <muluneh.derese@protonmail.com>
2022-01-29 19:03:48 +0300

    remove dist folder

--- dist/app.js
+++
@@ -1,5 +0,0 @@
-import { ProjectInput } from "./component/project-input.js";
-import { ProjectList } from "./component/project-list.js";
-new ProjectInput();
-new ProjectList("active");
-new ProjectList("finished");
