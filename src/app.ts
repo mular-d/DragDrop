5c8915a0335b51c64f2b79297d62521a2aeb72a7
Muluneh Derese <muluneh.derese@protonmail.com>
2022-05-08 09:29:02 +0300

    using vite for bundler

--- src/app.ts
+++
@@ -1,6 +0,0 @@
-import { ProjectInput } from "./component/project-input";
-import { ProjectList } from "./component/project-list";
-
-new ProjectInput();
-new ProjectList("active");
-new ProjectList("finished");
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { ProjectInput } from './component/project-input'
+import { ProjectList } from './component/project-list'
+
+new ProjectInput()
+new ProjectList('active')
+new ProjectList('finished')
