9370059810b22added5ef015e8565b20786d8673
Muluneh Derese <muluneh.derese@protonmail.com>
2022-05-08 08:47:17 +0300

    build check

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,25 +1,25 @@
-const path = require("path");
-const CleanPlugin = require("clean-webpack-plugin");
+const path = require('path')
+const CleanPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-  mode: "production",
-  entry: "./src/app.ts",
+  mode: 'production',
+  entry: './src/app.ts',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
   },
-  devtool: "nosources-source-map",
+  devtool: 'nosources-source-map',
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: [".ts", ".js"],
+    extensions: ['.ts', '.js'],
   },
   plugins: [new CleanPlugin.CleanWebpackPlugin()],
-};
+}
